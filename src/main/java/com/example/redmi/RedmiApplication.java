20e2635cbd6099092c8d5e47f8ff1b8eb84dcc7e
RKnagre <nagrek205@gmail.com>
2023-05-25 19:26:53 +0530

    codepush

---
+++ src/main/java/com/example/redmi/RedmiApplication.java
@@ -0,0 +1,13 @@
+package com.example.redmi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedmiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedmiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/redmi/RedmiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.redmi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedmiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
